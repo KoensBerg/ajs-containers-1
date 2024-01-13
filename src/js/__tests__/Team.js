859b3d2ef331d2aad4268ae19a066480e52473c3
Oleg Yanke <yanke.kd@gmail.com>
2024-01-13 20:24:46 +0200

    small corrections

--- src/js/__tests__/Team.js
+++ src/js/__tests__/Team.js
@@ -2,14 +2,14 @@ import Bowman from '../Bowman';
 import Swordsman from '../Swordsman';
 import Team from '../Team';
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Если персонаж уже добавлен в команду, выбросит ошибку
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Если игрок уже добавлен в команду, выбросит ошибку
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
 test('throws on incorrect character addition', () => {
   const Max = new Swordsman('Max');
   const newTeam = new Team();
 
-  newTeam.add(Max); // добавили персонажа в команду
+  newTeam.add(Max); // добавили игрока в команду
 
   const received = () => newTeam.add(Max); // добавили повторно
   const expected = 'Этот персонаж уже был добавлен в команду';
@@ -17,9 +17,9 @@ test('throws on incorrect character addition', () => {
   expect(received).toThrow(expected);
 });
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Последовательно добавляем неповторяющихся персонажей
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - -
+// Последовательно добавляем уникальных игроков
+// - - - - - - - - - - - - - - - - - - - - - - -
 test('should receive a set of three objects', () => {
   const Rob = new Bowman('Rob');
   const Tim = new Bowman('Tim');
@@ -37,16 +37,16 @@ test('should receive a set of three objects', () => {
   expect(received).toBe(expected);
 });
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Добавляем персонажей одной строкой, включая одинаковых
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Добавляем игроков одной строкой, включая одинаковых
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
 test('should add identical characters without error', () => {
   const Rob = new Bowman('Rob');
   const Tim = new Bowman('Tim');
   const Max = new Swordsman('Max');
 
   const newTeam = new Team();
-  newTeam.addAll(Rob, Tim, Tim, Max);
+  newTeam.addAll(Rob, Tim, Tim, Max); // попытка добавить четырёх игроков
 
   const received = newTeam.members.size; // размер Set
   const expected = 3; // в Set находится три объекта
@@ -65,7 +65,7 @@ test('should receive array of objects', () => {
   const newTeam = new Team();
   newTeam.addAll(Rob, Tim, Max);
 
-  const received = newTeam.toArray();
+  const received = newTeam.toArray(); // преобразуем в массив
   const expected = [Rob, Tim, Max]; // массив из трёх объектов
 
   expect(received).toStrictEqual(expected);
